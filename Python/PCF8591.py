69b862572d1433c258d1baf351a3aa9b6ccc7982
Cavon <lijiarong@sunfounder.com>
2016-12-28 10:44:31 +0800

    add debug info

--- Python/PCF8591.py
+++ Python/PCF8591.py
@@ -28,22 +28,30 @@ def setup(Addr):
 	address = Addr
 
 def read(chn): #channel
-	if chn == 0:
-		bus.write_byte(address,0x40)
-	if chn == 1:
-		bus.write_byte(address,0x41)
-	if chn == 2:
-		bus.write_byte(address,0x42)
-	if chn == 3:
-		bus.write_byte(address,0x43)
-	bus.read_byte(address) # dummy read to start conversion
+	try:
+		if chn == 0:
+			bus.write_byte(address,0x40)
+		if chn == 1:
+			bus.write_byte(address,0x41)
+		if chn == 2:
+			bus.write_byte(address,0x42)
+		if chn == 3:
+			bus.write_byte(address,0x43)
+		bus.read_byte(address) # dummy read to start conversion
+	except Exception, e:
+		print "Address: %s" % address
+		print e
 	return bus.read_byte(address)
 
 def write(val):
-	temp = val # move string value to temp
-	temp = int(temp) # change string to integer
-	# print temp to see on terminal else comment out
-	bus.write_byte_data(address, 0x40, temp)
+	try:
+		temp = val # move string value to temp
+		temp = int(temp) # change string to integer
+		# print temp to see on terminal else comment out
+		bus.write_byte_data(address, 0x40, temp)
+	except Exception, e:
+		print "Address: %s" % address
+		print e
 
 if __name__ == "__main__":
 	setup(0x48)
