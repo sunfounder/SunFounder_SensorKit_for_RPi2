104ef4f62fb7e8582c62e5bb428550c18edaef2b
daisy <daisy@example.com>
2018-01-05 02:26:48 +0000

    “comment”

--- Python/16_potentiometer.py
+++ Python/16_potentiometer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 import PCF8591 as ADC
-import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BCM)
 
 def setup():
 	ADC.setup(0x48)
@@ -12,12 +9,20 @@ def loop():
 	status = 1
 	while True:
 		print 'Value:', ADC.read(0)
-		
+		Value = ADC.read(0)
+		outvalue = map(Value,0,255,120,255)
+                ADC.write(outvalue)
 		time.sleep(0.2)
+def destroy():
+	ADC.write(0)
+
+def map(x, in_min, in_max, out_min, out_max):
+        '''To map the value from arange to another'''
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 if __name__ == '__main__':
 	try:
 		setup()
 		loop()
 	except KeyboardInterrupt: 
-		pass	
+		destroy()
