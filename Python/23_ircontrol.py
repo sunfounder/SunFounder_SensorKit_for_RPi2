ed88539bddcc0f77e0e75436cb8a3f40d7c5f66c
xiemeiping <1035791926@qq.com>
2022-09-29 16:46:55 +0800

    change the lirc

--- Python/23_ircontrol.py
+++ Python/23_ircontrol.py
@@ -1,22 +1,31 @@
-#!/usr/bin/python3
-
-import pylirc
+import lirc
 import time
 import RPi.GPIO as GPIO
 
+# client = lirc.Client()
+# print(client.version())
+
+''' RGB config'''
 Rpin = 17
 Gpin = 18
 Bpin = 27
-blocking = 0
 
 Lv = [0, 20, 90] # Light Level
 color = [0, 0, 0]
+
+p_R = None
+p_G = None
+p_B = None
+
 
 def setColor(color):
 	# global p_R, p_G, p_B
 	p_R.ChangeDutyCycle(100 - color[0])     # Change duty cycle
 	p_G.ChangeDutyCycle(100 - color[1])
 	p_B.ChangeDutyCycle(100 - color[2])
+
+def x():
+    setColor(color)
 
 def setup():
 	global p_R, p_G, p_B
@@ -32,66 +41,64 @@ def setup():
 	p_R.start(100)
 	p_G.start(100)
 	p_B.start(100)
-	err = pylirc.init("ircontrol", "./lircrc", blocking)
-	print(err)
-	if (err == 0):
-		raise IOError("IR init error!")
 
 def map(x, in_min, in_max, out_min, out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def RGB(config):
-	global color
-	if config == 'KEY_CHANNELDOWN':
-		color[0] = Lv[0]
-		print ('Red OFF')
+def key_handler(key:str):
+    global color
 
-	if config == 'KEY_CHANNEL':
-		color[0] = Lv[1]
-		print ('Light Red')
+    if key == 'KEY_CHANNELDOWN':
+        color[0] = Lv[0]
+        print ('Red OFF')
 
-	if config == 'KEY_CHANNELUP':
-		color[0] = Lv[2]
-		print ('Red')
+    elif key == 'KEY_CHANNEL':
+        color[0] = Lv[1]
+        print ('Dark Red')
 
-	if config == 'KEY_PREVIOUS':
-		color[1] = Lv[0]
-		print ('Green OFF')
+    elif key == 'KEY_CHANNELUP':
+        color[0] = Lv[2]
+        print ('Bright Red')
 
-	if config == 'KEY_NEXT':
-		color[1] = Lv[1]
-		print ('Light Green')
+    elif key == 'KEY_PREVIOUS':
+        color[1] = Lv[0]
+        print ('Green OFF')
 
-	if config == 'KEY_PLAYPAUSE':
-		color[1] = Lv[2]
-		print ('Green')
+    elif key == 'KEY_NEXT':
+        color[1] = Lv[1]
+        print ('Dark Green')
 
-	if config == 'KEY_VOLUMEDOWN':
-		color[2] = Lv[0]
-		print ('Blue OFF')
+    elif key == 'KEY_PLAYPAUSE':
+        color[1] = Lv[2]
+        print ('Bright Green')
 
-	if config == 'KEY_VOLUMEUP':
-		color[2] = Lv[1]
-		print ('Light Blue')
+    elif key == 'KEY_VOLUMEDOWN':
+        color[2] = Lv[0]
+        print ('Blue OFF')
 
-	if config == 'KEY_EQUAL':
-		color[2] = Lv[2]
-		print ('BLUE')
+    elif key == 'KEY_VOLUMEUP':
+        color[2] = Lv[1]
+        print ('Dark Blue')
 
+    elif key == 'KEY_EQUAL':
+        color[2] = Lv[2]
+        print ('Bright BLUE')
+
+    setColor(color)
+
+    
 def loop():
-	while True:
-		s = pylirc.nextcode(1)
-		# print(s)
-		while(s):
-			for (code) in s:
-				print ("Command: ", code["config"]) #For debug: Uncomment this
-#				line to see the return value of buttons
-				RGB(code["config"])
-				setColor(color)
-			if(not blocking):
-				s = pylirc.nextcode(1)
-			else:
-				s = []
+    with lirc.LircdConnection(timeout=5.0) as conn:
+        conn.connect()
+        while True:
+            try: 
+                # print(conn.readline()) # 0000000000000001 00 KEY_CHANNELDOWN ./lircd.conf
+                key = conn.readline().split(' ')[2] #KEY_CHANNELDOWN
+                # print(key)
+                key_handler(key)
+            except TimeoutError:
+                # print('Timeout')
+                pass 
 
 def destroy():
 	p_R.stop()
@@ -101,12 +108,13 @@ def destroy():
 	GPIO.output(Gpin, GPIO.HIGH)
 	GPIO.output(Bpin, GPIO.HIGH)
 	GPIO.cleanup()
-	pylirc.exit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	try:
 		setup()
 		loop()
 	except KeyboardInterrupt:
 		destroy()
+        
 
